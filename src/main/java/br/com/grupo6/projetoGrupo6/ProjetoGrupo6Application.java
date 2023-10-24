38c300bc1a6baa3b39b3b37df7e5130f4ab11886
Guilherme Alleoni <guilherme.alleoni@yahoo.com.br>
2023-10-23 21:54:20 -0300

    Iniciando Projeto

---
+++ src/main/java/br/com/grupo6/projetoGrupo6/ProjetoGrupo6Application.java
@@ -0,0 +1,13 @@
+package br.com.grupo6.projetoGrupo6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGrupo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGrupo6Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/grupo6/projetoGrupo6/ProjetoGrupo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grupo6.projetoGrupo6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGrupo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
